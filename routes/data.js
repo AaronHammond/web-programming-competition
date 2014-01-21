83edcb991bdf834340e2d36ba7eed2b320f5d2a5
Aaron Hammond <ahammond@mit.edu>
2014-01-21 15:02:40 -0500

    Preferences are now used in data return

--- routes/data.js
+++ routes/data.js
@@ -1,3 +1,6 @@
+Preferences = require("../models/Preferences");
+
+
 var yelp = require('yelp').createClient({
 	consumer_key: '433m34eZbqADW0sW9DqBuQ', 
   	consumer_secret: 'sG_Xp_MqAT_Vd80Q3GcNSRBLKa4',
@@ -8,18 +11,49 @@ var yelp = require('yelp').createClient({
 
 exports.getRestaurants = function (req, res) {
 	yelp.search({radius_filter: 1500, category_filter: "restaurants", sort: "0", ll: req.param('lat') + ',' + req.param('long')}, function(error, data) {
-	  res.send(data);
+		cullAndPassThru(data, req, res);
 	});
 }
 
 exports.getParks = function (req, res) {
 	yelp.search({radius_filter: 1500, category_filter: "parks,landmarks,galleries,museums", sort: "0", ll: req.param('lat') + ',' + req.param('long')}, function(error, data) {
-	  res.send(data);
+		cullAndPassThru(data, req, res);
 	});
 }
 
 exports.getBars = function (req, res) {
 	yelp.search({radius_filter: 1500, category_filter: "bars", sort: "0", ll: req.param('lat') + ',' + req.param('long')}, function(error, data) {
-	  res.send(data);
+		cullAndPassThru(data, req, res);
 	});
+}
+
+function cullAndPassThru(data, req, res){
+	if(!req.user){
+		return res.redirect('/user/login');
+	}
+
+	Preferences.findOne({owner: req.user._id}, function(err, doc){
+		var mesh = doc.preferences
+		var revisedDataset = [];
+		for(var i = 0; i < data.businesses.length; i++){
+			var biz = data.businesses[i];
+			if(shouldPushOut(mesh, biz.categories)){
+				revisedDataset.push(biz);
+			}
+		}
+		res.send({businesses : revisedDataset});
+	});
+}
+
+function shouldPushOut(mesh, categories){
+	for(var j = 0; j < categories.length; j++){
+		var pair = categories[j];
+		for(var k = 0; k < mesh.length; k++){
+			if(pair[1] == mesh[k].subcategory){
+				console.log(mesh[k]);
+				return (mesh[k].preference != "no");
+			}
+		}
+	}
+	return true;
 }
--- routes/map.js
+++ routes/map.js
@@ -1,4 +1,8 @@
 
 exports.viewMap = function(req, res) {
+	if(!req.user){
+		return res.redirect('/user/login');
+	}
+
 	res.render('map');
 }
